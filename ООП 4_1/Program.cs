6a2893e08e5ca1c2b83e89523f0fee80eb6b9dc7
Андрей Галеев <galeev.kamensk33@mail.ru>
2020-06-24 06:04:55 +0500

    Добавьте файлы проекта.

---
+++ ООП 4_1/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ООП_4_1
+{
+    class Health
+    {
+        public int Temp;
+        public int Temperature
+        {
+            get
+            {
+                return Temp;
+            }
+            set
+            {
+                if (value < 34 || value > 41)
+                    Console.WriteLine("Неверный ввод температуры");
+                else
+                    Console.WriteLine("Ваша температура = " + value);
+            }
+        }
+        class Program
+    {
+            static void Main(string[] args)
+            {
+                Console.Write("Введите температуру больше 34 и менее 41: ");
+                int NumTemp = Convert.ToInt32(Console.ReadLine());
+                Health newTem = new Health();
+                newTem.Temperature = NumTemp;
+                Console.ReadLine(); 
+            }
+        }
+}
+}
